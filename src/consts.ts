03a717030326c5446bfc57d304292323f2d97c02
Anderson Castellon <anderson.ivan.fuentes@gmail.com>
2023-06-26 08:49:53 -0600

    ✨ Add boilerplate page

--- src/consts.ts
+++ src/consts.ts
@@ -1,66 +1,69 @@
 export const SITE = {
-  title: "Web Development",
-  description: "JDK Web Development Workflow Website.",
-  defaultLanguage: "en-us",
-} as const;
+  title: 'Web Development',
+  description: 'JDK Web Development Workflow Website.',
+  defaultLanguage: 'en-us'
+} as const
 
 export const OPEN_GRAPH = {
   image: {
-    src: "https://github.com/HackMort/jdk-workflow/blob/359bea5640e6b785be658ea03c83bd38b8205ec4/public/logo.png?raw=true",
-    alt: "JDK Outstanding Technologies Logo ",
+    src: 'https://github.com/HackMort/jdk-workflow/blob/359bea5640e6b785be658ea03c83bd38b8205ec4/public/logo.png?raw=true',
+    alt: 'JDK Outstanding Technologies Logo '
   },
-  twitter: "astrodotbuild",
-};
+  twitter: 'astrodotbuild'
+}
 
 export const KNOWN_LANGUAGES = {
-  English: "en",
-} as const;
-export const KNOWN_LANGUAGE_CODES = Object.values(KNOWN_LANGUAGES);
-export const GITHUB_EDIT_URL = `https://github.com/withastro/astro/tree/main/examples/docs`;
+  English: 'en'
+} as const
+export const KNOWN_LANGUAGE_CODES = Object.values(KNOWN_LANGUAGES)
+export const GITHUB_EDIT_URL = `https://github.com/withastro/astro/tree/main/examples/docs`
 
 // See "Algolia" section of the README for more information.
 export const ALGOLIA = {
-  indexName: "tech-jdoutstanding",
-  appId: "AKCUAKGYAJ",
-  apiKey: "9344bf64fb52846c58b95a2428f3a697",
-};
+  indexName: 'tech-jdoutstanding',
+  appId: 'AKCUAKGYAJ',
+  apiKey: '9344bf64fb52846c58b95a2428f3a697'
+}
 
 export type Sidebar = Record<
   (typeof KNOWN_LANGUAGE_CODES)[number],
   Record<string, { text: string; link: string }[]>
->;
+>
 export const SIDEBAR: Sidebar = {
   en: {
     Workflow: [
-      { text: "Introduction", link: "en/introduction" },
-      { text: "Analysis", link: "en/analysis" },
-      { text: "Codebase", link: "en/codebase" },
-      { text: "Components", link: "en/components" },
-      { text: "Pages", link: "en/pages" },
-      { text: "Final Steps", link: "en/final-steps" },
+      { text: 'Introduction', link: 'en/introduction' },
+      { text: 'Analysis', link: 'en/analysis' },
+      { text: 'Codebase', link: 'en/codebase' },
+      { text: 'Components', link: 'en/components' },
+      { text: 'Pages', link: 'en/pages' },
+      { text: 'Final Steps', link: 'en/final-steps' }
     ],
-    "Local Environment": [
-      { text: "Introduction", link: "en/local-intro" },
+    'Local Environment': [
+      { text: 'Introduction', link: 'en/local-intro' },
       {
-        text: "How to install VSCode on Windows 11",
-        link: "en/install-vscode-windows-11",
+        text: 'How to install VSCode on Windows 11',
+        link: 'en/install-vscode-windows-11'
       },
       {
-        text: "How to install GIT on Windows 11",
-        link: "en/install-git-windows-11",
+        text: 'How to install GIT on Windows 11',
+        link: 'en/install-git-windows-11'
       },
       {
-        text: "How to install nodejs on Windows 11",
-        link: "en/install-nodejs-windows-11",
-      },
+        text: 'How to install nodejs on Windows 11',
+        link: 'en/install-nodejs-windows-11'
+      }
+    ],
+    'Getting starter': [
+      { text: 'Proyects boilerplate', link: 'en/proyects-boilerplate' }
     ],
     Resources: [
-      { text: "BEM", link: "en/bem" },
-      { text: "Git", link: "en/git" },
-      { text: "ESLint", link: "en/eslint" },
-      { text: "Stylelint", link: "en/stylelint" },
-      { text: "Assets compression", link: "en/assets-compression" },
-      { text: "Tools", link: "en/tools" }
-    ],
-  },
-};
+      { text: 'BEM', link: 'en/bem' },
+      { text: 'Git', link: 'en/git' },
+      { text: 'ESLint', link: 'en/eslint' },
+      { text: 'Stylelint', link: 'en/stylelint' },
+      { text: 'Assets compression', link: 'en/assets-compression' },
+      { text: 'Tools', link: 'en/tools' }
+    ]
+  }
+}
---
+++ src/content/docs/en/proyects-boilerplate.md
@@ -0,0 +1,26 @@
+---
+title: "Accelerate Your Development with the JDK Boilerplate"
+description: "Discover the power of the JDK Boilerplate in streamlining your development process. This comprehensive blog post guides you through cloning the repository, installing dependencies, and running the project. Learn about essential commands, notes, customizations, and components, as well as utilizing Astro and VS Code extensions for enhanced efficiency."
+image:
+  {
+    src: /images/carl-heyerdahl-KE0nC8-58MQ-unsplash.jpg,
+    attribution:Photo by <a href="https://unsplash.com/@carlheyerdahl?utm_source=unsplash&utm_medium=referral&utm_content=creditCopyText">Carl Heyerdahl</a> on <a href="https://unsplash.com/photos/KE0nC8-58MQ?utm_source=unsplash&utm_medium=referral&utm_content=creditCopyText">Unsplash</a>,
+  }
+---
+
+1. **Easy Setup and Installation**:
+   Clone the JDK Boilerplate repository and quickly set up your project with the provided commands. Learn how to install dependencies and run the project effortlessly.
+
+2. **Convenient Commands and Configurations**:
+   Explore the wide range of commands available to streamline your development workflow. From installing dependencies and building your production site to previewing your changes locally, leverage the power of predefined commands for increased productivity.
+
+3. **Customization and Component Creation**:
+   Learn how to customize the boilerplate to meet your project requirements. Create components with ease by following step-by-step instructions, and understand how to import styles, configure modal instances, and listen to modal events effectively.
+
+4. **Boosting Efficiency with Astro Extensions**:
+   Discover the Astro extension for Visual Studio Code, providing syntax highlighting and convenient code snippets. Install the Astro Snippets extension for quick and easy component creation, saving valuable time during development.
+
+5. **Ensuring Code Quality and Consistency**:
+   Utilize the provided ESLint, Prettier, and Stylelint configurations to maintain code quality and adhere to best practices. Learn about available resources for further reference on configuration options and style rules.
+
+> Visit the <a href="https://github.com/HackMort/jdk-boilerplate/tree/develop" target="_blank">JDK Boilerplate repository</a> and discover how this boilerplate can save you time and effort.
